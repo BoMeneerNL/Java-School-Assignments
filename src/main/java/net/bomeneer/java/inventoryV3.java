01f54c7930a23248f14505ffc58680d365709d2a
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-03-19 00:32:17 +0100

    update 5

--- src/main/java/net/bomeneer/java/inventoryV3.java
+++ src/main/java/net/bomeneer/java/inventoryV3.java
@@ -3,6 +3,7 @@ package net.bomeneer.java;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import static java.lang.System.out; import static java.lang.System.in;
+import static net.bomeneer.java.invertoryV2.divider_s;
 
 public class inventoryV3 {
 
@@ -20,21 +21,128 @@ public class inventoryV3 {
     static products minecraftb = new products(23.95, "Games (prepaid cards)", "Minecraft: Bedrock Edition (Prepaid card)", 20, "Windows, MacOS, Linux", 7);
     static products gtav = new products(50.55,"Games (Physical Copy)","GTA V", 10, "PlayStation 3, Xbox 360, PlayStation 4, Xbox One, Windows, PlayStation 5, Xbox Series X", 18);
     static products balpen = new products(20.10,"Pennen","Balpen",500);
-    static String[] newproduct1;
+    static String[][] newproducts = {
+            /*NewProduct 1*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 2*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 3*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 4*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 5*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 6*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 7*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 8*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 9*/{"0.00","ProductType","Product","Stocked"},
+            /*NewProduct 10*/{"0.00","ProductType","Product","Stocked"}
+        };
 
     public static void main(){
-        if(newitems > 0 && newitems < 10){
+        if(newitems > 0 && newitems <= 10){
             do {
-                out.println("what is the price of the new product");
+                out.println("what is the price of the new product?, use price as '0.00' prices are in EUR(€)");
+                while(newproducts[newitems - 1][0] == "0.00" || newproducts[newitems - 1][0] == "") {
+                    newproducts[newitems - 1][0] = input.nextLine();
+                }
+
+                out.println("What is the product type of the new product?");
+                while(newproducts[newitems - 1][1] == "ProductType") {
+                    newproducts[newitems - 1][1] = input.nextLine();
+                }
+                out.println("What is the (product) name of the new product?");
+                while(newproducts[newitems - 1][2] == "Product") {
+                    newproducts[newitems - 1][2] = input.nextLine();
+                }
+                out.println("How many items of the new product are stocked?");
+                while(newproducts[newitems - 1][3] == "Stocked") {
+                    newproducts[newitems - 1][3] = input.nextLine();
+                }
                 newitems--;
             }while(newitems != 0);
+            if(newproducts[0][1] != "ProductType" && newproducts[0][2] != "Product" && newproducts[0][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[0][0] + "€");
+                out.println("Product Type: " + newproducts[0][1]);
+                out.println("Product: " + newproducts[0][2]);
+                out.println("Stocked: " + newproducts[0][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[1][1] != "ProductType" && newproducts[1][2] != "Product" && newproducts[1][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[1][0] + "€");
+                out.println("Product Type: " + newproducts[1][1]);
+                out.println("Product: " + newproducts[1][2]);
+                out.println("Stocked: " + newproducts[1][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[2][1] != "ProductType" && newproducts[2][2] != "Product" && newproducts[2][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[2][0] + "€");
+                out.println("Product Type: " + newproducts[2][1]);
+                out.println("Product: " + newproducts[2][2]);
+                out.println("Stocked: " + newproducts[2][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[3][1] != "ProductType" && newproducts[3][2] != "Product" && newproducts[3][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[3][0] + "€");
+                out.println("Product Type: " + newproducts[3][1]);
+                out.println("Product: " + newproducts[3][2]);
+                out.println("Stocked: " + newproducts[3][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[4][1] != "ProductType" && newproducts[4][2] != "Product" && newproducts[4][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[4][0] + "€");
+                out.println("Product Type: " + newproducts[4][1]);
+                out.println("Product: " + newproducts[4][2]);
+                out.println("Stocked: " + newproducts[4][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[5][1] != "ProductType" && newproducts[5][2] != "Product" && newproducts[5][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[5][0] + "€");
+                out.println("Product Type: " + newproducts[5][1]);
+                out.println("Product: " + newproducts[5][2]);
+                out.println("Stocked: " + newproducts[5][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[6][1] != "ProductType" && newproducts[6][2] != "Product" && newproducts[6][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[6][0] + "€");
+                out.println("Product Type: " + newproducts[6][1]);
+                out.println("Product: " + newproducts[6][2]);
+                out.println("Stocked: " + newproducts[6][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[7][1] != "ProductType" && newproducts[7][2] != "Product" && newproducts[7][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[7][0] + "€");
+                out.println("Product Type: " + newproducts[7][1]);
+                out.println("Product: " + newproducts[7][2]);
+                out.println("Stocked: " + newproducts[7][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[8][1] != "ProductType" && newproducts[8][2] != "Product" && newproducts[8][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[8][0] + "€");
+                out.println("Product Type: " + newproducts[8][1]);
+                out.println("Product: " + newproducts[8][2]);
+                out.println("Stocked: " + newproducts[8][3]);
+                out.println(divider_s);
+            }
+            if(newproducts[9][1] != "ProductType" && newproducts[9][2] != "Product" && newproducts[9][3] != "Stocked"){
+                out.println(divider_s);
+                out.println("Price: " + newproducts[9][0] + "€");
+                out.println("Product Type: " + newproducts[9][1]);
+                out.println("Product: " + newproducts[9][2]);
+                out.println("Stocked: " + newproducts[9][3]);
+                out.println(divider_s);
+            }
             minecraftb.Outputter();
             minecraftj.Outputter();
             gtav.Outputter();
             balpen.Outputter();
         }
         else if(newitems > 10){
-
+            out.println("oops, to much new products");
         }
         else{
             minecraftb.Outputter();
@@ -63,7 +171,7 @@ public class inventoryV3 {
                 main();
                 jeff = 1;
             }
-            //the if newitems <= 0 is handled @ main();
+            //the if newitems <= 0 is handled @ main(){}
         } while (jeff == 0);
     }
     public inventoryV3(float price, String producttype, String product, int stocked){
