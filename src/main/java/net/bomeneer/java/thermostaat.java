df30cae3dce84a0b01f84c0e2fc0d7bc88f315c4
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-03-25 14:58:13 +0100

    update 7

--- src/main/java/net/bomeneer/java/thermostaat.java
+++ src/main/java/net/bomeneer/java/thermostaat.java
@@ -3,32 +3,57 @@ package net.bomeneer.java;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import static java.lang.System.out;
+import static java.lang.System.*;
 
 public class thermostaat {
     static String command;
-    static int settedtempinterval;
+    static byte settedtempinterval;
+    static float nowtemp = 20;
     public static Scanner input = new Scanner(System.in);
-    static void changetemp(byte tempinterval, double oldtemp,double nowtemp, double newtemp,boolean tempup){
+    static void changetemp(byte tempinterval, double oldtemp,float nowtemp, float newtemp,boolean tempup){
         if(tempup){
 
         }
         else if(!tempup){
             while(nowtemp > newtemp){
                 nowtemp = nowtemp - tempinterval;
+                out.println(nowtemp);
             }
+
         }
         else {
             out.println("Oops, something went wrong :(");
         }
     }
     static void main(){
-        while(command != "connect 192.168.1.99 --login=thermostaat,1234") {
-            out.print("Welcome to SRCLA, what do you want to do?");
-            command = input.nextLine();
-        }
+        out.println("Welcome to SRCLA?");
         while(command != "exit" || command != "end"){
-
+            out.print("SRCLA?/Thermostaat@192.168.1.99>");
+            command = input.nextLine();
+            switch (command){
+                case "setinterval":
+                    out.println("To what interval do you want to set your thermostat? (only use full numbers)");
+                    out.println("SRCLA?/Thermostaat@192.168.1.99/SetInterval>");
+                    try {
+                        settedtempinterval = input.nextByte();
+                        out.print("");
+                    }
+                    catch (InputMismatchException exception){
+                        out.println("Oops, this command only accepts between -127 and 127 and doesn't support decimal numbers");
+                    }
+                break;
+                case "settemp":
+                    out.println("What");
+                    break;
+                case "icall geti":
+                    out.println("VAR$settedtempinterval = " + settedtempinterval);
+                    break;
+                case "exit":
+                case "end":
+                    System.exit(0);
+                    break;
+                default:
+            }
         }
     }
 }
