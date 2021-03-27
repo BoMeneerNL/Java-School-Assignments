1cfdbac590382f5daadadb307e073badb57a26a0
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-03-27 01:00:55 +0100

    update 14

--- src/main/java/net/bomeneer/java/thermostaat.java
+++ src/main/java/net/bomeneer/java/thermostaat.java
@@ -2,6 +2,7 @@ package net.bomeneer.java;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.time.LocalDateTime;
 
 import static java.lang.System.*;
 
@@ -12,9 +13,21 @@ public class thermostaat {
     static float newtemp; //Contains
     static boolean proceed = true; //Controlls Exit@error&leaving
     static boolean tempup; //acts as temperature up or down in changetemp()
-    String[][] history = { {null, null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null}, {null, null, null} };
+    static int begindayhour; //holds data with the beginning hour time of the DAY temperature
+    static int begindayminute; //Same as "begindayhour" but then the minutes
+    static boolean enableddaytime = false; //Read if the user enabled or disabled Day-Night time based temperature
+    String[][] history = { {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null} };
     public static Scanner input = new Scanner(System.in);
+    static LocalDateTime now = LocalDateTime.now();
+    static void sethistory(String action,int day,int month,int year,int hour,int minute,int second){
+
+
+    }
+    static void gethistory(int historycounter){
+
+    }
     static void changetemp(byte tempinterval, double oldtemp, float tempnewtemp, boolean temptempup){
+        sethistory("changed temperature",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
         if(temptempup){
             while(nowtemp < tempnewtemp){
                 nowtemp = nowtemp + tempinterval;
@@ -38,6 +51,7 @@ public class thermostaat {
         poweron();
     }
     static void poweron(){
+        sethistory("Powered on System",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
         while(proceed){
             out.print("SRCLA?/Thermostaat@192.168.1.99>");
             command = input.nextLine();
@@ -55,13 +69,15 @@ public class thermostaat {
                 break;
                 case "settemp":
                     out.println("What do you want to be the new temperature?");
+                    out.print("SRCLA?/Thermostaat@192.168.1.99/SetNewTemp>");
                     newtemp = input.nextFloat();
                     if (newtemp > nowtemp){tempup = true;}
                     else if(newtemp < nowtemp){tempup = false;}
                     changetemp(settedtempinterval,nowtemp,newtemp,tempup);
                     break;
                 case "changetimes":
-
+                    out.println("to what hour do you want to start the DAY time temperature?");
+                    out.print("SRCLA?/Thermostaat@192.168.1.99/SetDayTime");
                 case "icall geti":
                     out.println("I(nternal)CALL GETInfo/VAR Dump: ");
                     out.println("VAR$command = " + command);
@@ -69,7 +85,10 @@ public class thermostaat {
                     out.println("VAR$nowtemp = " + nowtemp);
                     out.println("VAR$newtemp = " + newtemp);
                     out.println("VAR$proceed = " + proceed);
-                    out.println("VAR$tempup =" + tempup);
+                    out.println("VAR$tempup = " + tempup);
+                    out.println("VAR$begindayhour = " + begindayhour);
+                    out.println("VAR$begindayminute = " + begindayminute);
+                    out.println("VAR$enableddaytime = " + enableddaytime);
                     break;
                 case "poweroff":
                     poweroff();
@@ -83,13 +102,18 @@ public class thermostaat {
         }
     }
     static void poweroff(){
+        sethistory("Powered on System",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
         int jeff = 0;
         while (jeff == 0){
             out.print("SRCLA?/SSH@NotConnected>");
             command = input.nextLine();
             switch (command) {
                 case "poweron" -> poweron();
-                case "exit program" -> System.exit(0);
+                case "exit program" -> {
+                    System.exit(0);
+                    jeff=1;
+                }
+
             }
         }
 
