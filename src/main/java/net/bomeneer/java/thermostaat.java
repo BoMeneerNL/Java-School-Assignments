4a6b8d83302814eb84db4e81832aafa79b415ee3
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-03-27 11:48:17 +0100

    update 15

--- src/main/java/net/bomeneer/java/thermostaat.java
+++ src/main/java/net/bomeneer/java/thermostaat.java
@@ -4,30 +4,77 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.time.LocalDateTime;
 
-import static java.lang.System.*;
+import static java.lang.System.out;
 
 public class thermostaat {
     static String command; //Controlls CL Commands input
-    static byte settedtempinterval; //Contains
-    static float nowtemp = 20; //Contains
-    static float newtemp; //Contains
+    static byte settedtempinterval; //Contains tempinterval
+    static float nowtemp = 20; //Contains the temperature that it is right now
+    static float newtemp; //Contains the new temperature
     static boolean proceed = true; //Controlls Exit@error&leaving
     static boolean tempup; //acts as temperature up or down in changetemp()
     static int begindayhour; //holds data with the beginning hour time of the DAY temperature
     static int begindayminute; //Same as "begindayhour" but then the minutes
     static boolean enableddaytime = false; //Read if the user enabled or disabled Day-Night time based temperature
-    String[][] history = { {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null} };
+    static String[][] history = { {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null}, {null, null, null, null, null, null, null} };
     public static Scanner input = new Scanner(System.in);
     static LocalDateTime now = LocalDateTime.now();
-    static void sethistory(String action,int day,int month,int year,int hour,int minute,int second){
 
+    //Set history tactic = populate 10>0 if 0 is already populated terminate data of 10 and move 1 spot up
+    static void sethistory(String action, String day, String month, String year, String hour, String minute, String second){
+    int looply = 9;
+    boolean change = false;
+    while (looply >= 0){
+        if(history[looply][0] == null && history[looply][1] == null && history[looply][2] == null && history[looply][3] == null && history[looply][4] == null && history[looply][5] == null && history[looply][6] == null){
+            history[looply][0] = action;
+            history[looply][1] = day;
+            history[looply][2] = month;
+            history[looply][3] = year;
+            history[looply][4] = hour;
+            history[looply][5] = minute;
+            history[looply][6] = second;
+
+        }
+        if(history[looply][0] == action && history[looply][1] == day && history[looply][2] == month && history[looply][3] == year && history[looply][4] == hour && history[looply][5] == minute && history[looply][6] == second){
+            looply = -1;
+             change = true;
+        }
+        else{
+            looply--;
+        }
+    }
+        if(looply < 0){
+            int shuffle = 9;//shuffels history if no empty array is found
+            int shufflefrom;//Shuffels the old to shuffle (is always shuffle -1)
+            if (change){
+
+            }
+            else if(!change){
+                while(shuffle > 0){
+                    shufflefrom = shuffle - 1;
+                    history[shuffle][0] = history[shufflefrom][0];
+                    shuffle--;
+                }
+                history[0][0] = action;
+                history[0][1] = day;
+                history[0][2] = month;
+                history[0][3] = year;
+                history[0][4] = hour;
+                history[0][5] = minute;
+                history[0][6] = second;
+            }
+            else{out.println("oops, something went wrong, exiting");System.exit(1);}
+        }
+        else{out.println("oops, something went wrong, exiting");System.exit(1);}
 
     }
+
     static void gethistory(int historycounter){
 
     }
+
     static void changetemp(byte tempinterval, double oldtemp, float tempnewtemp, boolean temptempup){
-        sethistory("changed temperature",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
+        sethistory("changed temperature",String.valueOf(now.getDayOfMonth()),String.valueOf(now.getMonthValue()),String.valueOf(now.getYear()),String.valueOf(now.getHour()),String.valueOf(now.getMinute()),String.valueOf(now.getSecond()));
         if(temptempup){
             while(nowtemp < tempnewtemp){
                 nowtemp = nowtemp + tempinterval;
@@ -51,7 +98,7 @@ public class thermostaat {
         poweron();
     }
     static void poweron(){
-        sethistory("Powered on System",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
+        sethistory("Powered on System",String.valueOf(now.getDayOfMonth()),String.valueOf(now.getMonthValue()),String.valueOf(now.getYear()),String.valueOf(now.getHour()),String.valueOf(now.getMinute()),String.valueOf(now.getSecond()));
         while(proceed){
             out.print("SRCLA?/Thermostaat@192.168.1.99>");
             command = input.nextLine();
@@ -79,6 +126,7 @@ public class thermostaat {
                     out.println("to what hour do you want to start the DAY time temperature?");
                     out.print("SRCLA?/Thermostaat@192.168.1.99/SetDayTime");
                 case "icall geti":
+                    out.println("\n");
                     out.println("I(nternal)CALL GETInfo/VAR Dump: ");
                     out.println("VAR$command = " + command);
                     out.println("VAR$settedtempinterval = " + settedtempinterval);
@@ -89,6 +137,16 @@ public class thermostaat {
                     out.println("VAR$begindayhour = " + begindayhour);
                     out.println("VAR$begindayminute = " + begindayminute);
                     out.println("VAR$enableddaytime = " + enableddaytime);
+                    out.println("ARRAY.VAR$history[0] = " + history[0][0] + "," + history[0][1] + "," + history[0][2] + "," + history[0][3] + "," + history[0][4] + "," + history[0][5] + "," + history[0][6]);
+                    out.println("ARRAY.VAR$history[1] = " + history[1][0] + "," + history[1][1] + "," + history[1][2] + "," + history[1][3] + "," + history[1][4] + "," + history[1][5] + "," + history[1][6]);
+                    out.println("ARRAY.VAR$history[2] = " + history[2][0] + "," + history[2][1] + "," + history[2][2] + "," + history[2][3] + "," + history[2][4] + "," + history[2][5] + "," + history[2][6]);
+                    out.println("ARRAY.VAR$history[3] = " + history[3][0] + "," + history[3][1] + "," + history[3][2] + "," + history[3][3] + "," + history[3][4] + "," + history[3][5] + "," + history[3][6]);
+                    out.println("ARRAY.VAR$history[4] = " + history[4][0] + "," + history[4][1] + "," + history[4][2] + "," + history[4][3] + "," + history[4][4] + "," + history[4][5] + "," + history[4][6]);
+                    out.println("ARRAY.VAR$history[5] = " + history[5][0] + "," + history[5][1] + "," + history[5][2] + "," + history[5][3] + "," + history[5][4] + "," + history[5][5] + "," + history[5][6]);
+                    out.println("ARRAY.VAR$history[6] = " + history[6][0] + "," + history[6][1] + "," + history[6][2] + "," + history[6][3] + "," + history[6][4] + "," + history[6][5] + "," + history[6][6]);
+                    out.println("ARRAY.VAR$history[7] = " + history[7][0] + "," + history[7][1] + "," + history[7][2] + "," + history[7][3] + "," + history[7][4] + "," + history[7][5] + "," + history[7][6]);
+                    out.println("ARRAY.VAR$history[8] = " + history[8][0] + "," + history[8][1] + "," + history[8][2] + "," + history[8][3] + "," + history[8][4] + "," + history[8][5] + "," + history[8][6]);
+                    out.println("ARRAY.VAR$history[9] = " + history[9][0] + "," + history[9][1] + "," + history[9][2] + "," + history[9][3] + "," + history[9][4] + "," + history[9][5] + "," + history[9][6]);
                     break;
                 case "poweroff":
                     poweroff();
@@ -102,20 +160,24 @@ public class thermostaat {
         }
     }
     static void poweroff(){
-        sethistory("Powered on System",now.getDayOfMonth(),now.getMonthValue(),now.getYear(),now.getHour(),now.getMinute(),now.getSecond());
+        sethistory("Powered off System",String.valueOf(now.getDayOfMonth()),String.valueOf(now.getMonthValue()),String.valueOf(now.getYear()),String.valueOf(now.getHour()),String.valueOf(now.getMinute()),String.valueOf(now.getSecond()));
         int jeff = 0;
         while (jeff == 0){
             out.print("SRCLA?/SSH@NotConnected>");
             command = input.nextLine();
             switch (command) {
-                case "poweron" -> poweron();
-                case "exit program" -> {
+                case "poweron":
+                    poweron();
+                    break;
+                case "poweroff":
+                case "icall getti":
+                case "changetimes":
+                    out.println("Could not execute command, err_noncon");
+                    break;
+                case "exit program":
                     System.exit(0);
-                    jeff=1;
-                }
-
+                    jeff = 1;
             }
         }
-
     }
 }
