63763bddb6a7608dbde88ba5682f46feae961003
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2022-04-19 01:00:39 +0200

    update 30

--- src/main/java/net/bomeneer/java/dobblebackground.java
+++ src/main/java/net/bomeneer/java/dobblebackground.java
@@ -6,12 +6,8 @@ import java.util.Random;
 public class dobblebackground {
     static int flnow = 0;
     //dobblesteen waardes
-    static String t1 = "[---]\n[   ]\n[ * ]\n[---]";
-    static String t2 = "[---]\n[ * ]\n[ * ]\n[---]";
-    static String t3 = "[---]\n[  *]\n[ **]\n[---]";
-    static String t4 = "[---]\n[** ]\n[** ]\n[---]";
-    static String t5 = "[---]\n[ **]\n[***]\n[---]";
-    static String t6 = "[---]\n[***]\n[***]\n[---]";
+    static String[] dobbel6 = {"[---]\n[   ]\n[ * ]\n[---]","[---]\n[ * ]\n[ * ]\n[---]","[---]\n[  *]\n[ **]\n[---]","[---]\n[** ]\n[** ]\n[---]",
+            "[---]\n[ **]\n[***]\n[---]","[---]\n[***]\n[***]\n[---]"};
     static String dobble1 = "*********\n*     *\n *  ";
     static String dobble2 = "  *\n*      *\n*********";
     static String space = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
@@ -19,11 +15,13 @@ public class dobblebackground {
     static int uitkomst;
 
     public static void dobblie(int typethrow) throws InterruptedException {
-        if (typethrow == 0) uitkomst = random.nextInt(6);
-        else if (typethrow == 1) uitkomst = random.nextInt(100);
-        else {
+        switch (typethrow) {
+        case 0 -> uitkomst = random.nextInt(6);
+        case 1 -> uitkomst = random.nextInt(100);
+        default -> {
             System.out.println("Oops, an unfixable error has occured");
             System.exit(1);
+            }
         }
 
         uitkomst++;
@@ -33,29 +31,14 @@ public class dobblebackground {
         }
         forlooper();
         System.out.println(space);
-        switch (uitkomst) {
-            case 1 -> System.out.println(t1);
-            case 2 -> System.out.println(t2);
-            case 3 -> System.out.println(t3);
-            case 4 -> System.out.println(t4);
-            case 5 -> System.out.println(t5);
-            case 6 -> System.out.println(t6);
-        }
+        System.out.println(dobbel6[uitkomst]);
         if (uitkomst >= 7 && uitkomst <= 100) System.out.println(dobble1 + uitkomst + dobble2);
         System.out.println("You've thrown" + " " + uitkomst);
     }
 
     public static void forlooper() {
         for(int i=1;i <= 6;i++){
-            System.out.println(space);
-            switch (i) {
-                case 1 -> System.out.println(t1);
-                case 2 -> System.out.println(t2);
-                case 3 -> System.out.println(t3);
-                case 4 -> System.out.println(t4);
-                case 5 -> System.out.println(t5);
-                case 6 -> System.out.println(t6);
-            }
+            System.out.println(dobbel6[i]);
             try { Thread.sleep(200); }
             catch (InterruptedException ignored) {}
         }
